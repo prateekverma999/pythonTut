413c80439af069cd002d8d9bf21dea7bb4743617
ankita <ankita@gmail.com>
2022-01-29 06:12:16 +0000

    v3

---
+++ ankita.java
@@ -0,0 +1,17 @@
+public class ankita
+{
+ public static void main (String []args)
+  {
+    int number=562;
+      
+    if (number % 2 == 0)
+     {
+         System.out.println(" your number is even ");
+     }
+     
+    else
+    {
+      System.out.println(" your number is odd ");
+    }
+ }
+  }
